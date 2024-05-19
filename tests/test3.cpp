613568deccc939bbf5d14c1d9716f39cb62e63ed
Titanoboba <nik20052006@mail.ru>
2024-05-19 08:51:49 -0400

    Updated test2.cpp and test3.cpp

--- tests/test3.cpp
+++ tests/test3.cpp
@@ -6,18 +6,18 @@
 class ProxyMock : public Proxy{
     public:
     ProxyMock(DataBase*database) : Proxy(database){}
-    MOCK_METHOD(Login, void(std::string userName,std::string password));
-    MOCK_METHOD(Append, void(const std::string &data));
-    MOCK_METHOD(GetList, std::list<std::string>);
-    MOCK_METHOD(Truncate, void());
+    MOCK_CONST_METHOD2(Login, void(std::string userName, std::string password));
+    MOCK_METHOD1(Append, void(std::string));
+    MOCK_CONST_METHOD0(GetList, std::list<std::string>());
+    MOCK_METHOD0(Truncate, void());
 };
 
 TEST(Proxy, Mock){
-    DataBase* database;
-    database.append("IamTheFirstString");
-    database.append("IamTheSecondString");
-    database.append("IamTheThirdString");
-    ProxyMock prox(DataBase*database);
+    DataBase* database = new DataBase();
+    database->Append("IamTheFirstString");
+    database->Append("IamTheSecondString");
+    database->Append("IamTheThirdString");
+    ProxyMock prox(database);
     //prox.Login("Me", "MyPassword1234"); I am not logged in
     std::list<std::string> dummy
     EXPECT_EQ(prox.GetList(), dummy);
