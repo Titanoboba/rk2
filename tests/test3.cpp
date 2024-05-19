7d362cff1fc415a0b0a9a9bd382a8dd997de61a4
Titanoboba <nik20052006@mail.ru>
2024-05-19 08:53:10 -0400

    Updated test3.cpp

--- tests/test3.cpp
+++ tests/test3.cpp
@@ -19,7 +19,7 @@ TEST(Proxy, Mock){
     database->Append("IamTheThirdString");
     ProxyMock prox(database);
     //prox.Login("Me", "MyPassword1234"); I am not logged in
-    std::list<std::string> dummy
+    std::list<std::string> dummy;
     EXPECT_EQ(prox.GetList(), dummy);
     EXPECT_EQ(prox.Truncate(), void());
     EXPECT_EQ(prox.IsLogedIn(), false);
