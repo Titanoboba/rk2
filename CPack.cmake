95e354b270bf28a9e1e639ce765e4f5a6f27e4f5
Titanoboba <nik20052006@mail.ru>
2024-05-19 09:47:10 -0400

    Updated CPack.cmake

--- CPack.cmake
+++ CPack.cmake
@@ -2,15 +2,12 @@ include(InstallRequiredSystemLibraries)
 
 set(CPACK_PACKAGE_CONTACT eisnersandy2@gmail.com)
 set(CPACK_PACKAGE_VERSION ${PRINT_VERSION})
-set(CPACK_PACKAGE_NAME "solver")
+set(CPACK_PACKAGE_NAME "code")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "C++ library")
-set(CPACK_PACKAGE_PACK_NAME "solver-${PRINT_VERSION}")
+set(CPACK_PACKAGE_PACK_NAME "code-${PRINT_VERSION}")
 
 set(CPACK_SOURCE_INSTALLED_DIRECTORIES 
-  "${CMAKE_SOURCE_DIR}/solver_application; solver_application"
-  "${CMAKE_SOURCE_DIR}/solver_lib; solver_lib"
-  "${CMAKE_SOURCE_DIR}/formatter_ex_lib; formatter_ex_lib"
-  "${CMAKE_SOURCE_DIR}/formatter_lib; formatter_lib")
+  "${CMAKE_SOURCE_DIR}/code; code")
 
 set(CPACK_RESOURCE_FILE_README ${CMAKE_CURRENT_SOURCE_DIR}/README.md)
 
