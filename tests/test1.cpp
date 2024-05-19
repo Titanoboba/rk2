b49ebefa54409952e8e5ad98915933bb0fd7c2c4
Titanoboba <nik20052006@mail.ru>
2024-05-19 06:47:55 -0400

    Updated test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -6,17 +6,17 @@
 class ProxyMock : public Proxy{
     public:
     ProxyMock(DataBase*database) : Proxy(database){}
-    MOCK_METHOD(Login, void(std::string userName,std::string password));
-    MOCK_METHOD(Append, void(const std::string &data));
+    MOCK_METHOD(Login, (void(std::string userName,std::string password)));
+    MOCK_METHOD(Append, (void(const std::string &data)));
     MOCK_METHOD(GetList, std::list<std::string>);
     MOCK_METHOD(Truncate, void());
 };
 
 TEST(Proxy, Mock){
     DataBase* database;
-    database.append("IamTheFirstString");
-    database.append("IamTheSecondString");
-    database.append("IamTheThirdString");
+    database->append("IamTheFirstString");
+    database->append("IamTheSecondString");
+    database->append("IamTheThirdString");
     ProxyMock prox(DataBase*database);
     prox.Login("Me", "MyPassword1234");
     prox.Append("IamTheFourthString");
