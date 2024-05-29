c155c39e5d2771346f41d534fb9948db63f84339
Titanoboba <79159681+Titanoboba@users.noreply.github.com>
2024-05-29 10:04:55 +0200

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -6,7 +6,7 @@
 class ProxyMock : public Proxy{
     public:
     ProxyMock(DataBase*database) : Proxy(database){}
-    MOCK_CONST_METHOD2(Login, void(std::string userName, std::string password));
+    MOCK_CONST_METHOD2(Login, void(std::string, std::string));
     MOCK_METHOD1(Append, void(std::string));
     MOCK_CONST_METHOD0(GetList, std::list<std::string>());
     MOCK_METHOD0(Truncate, void());
